75586bdb2a068460413e0c41fd100de77be1cfce
Satyam Tiwary <satyam@technoculture.io>
2023-06-11 23:38:28 +0530

    build issue

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,5 +3,10 @@ import { auth } from "@clerk/nextjs";
 
 export default async function Page() {
   const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
   redirect(`/${userId}`);
 }
