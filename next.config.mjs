b24bf223d53212de9a92c59dc8cebdedf9a2a78e
Satyam Tiwary <satyam@technoculture.io>
2023-06-11 22:59:26 +0530

    build issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,3 @@
-import { withSentryConfig } from '@sentry/nextjs';
 import "./src/app/env.mjs";
 
 /** @type {import('next').NextConfig} */
@@ -18,31 +17,4 @@ const nextConfig = {
   },
 };
 
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
-  // Suppresses source map uploading logs during build
-  silent: true,
-
-  org: "technoculture",
-  project: "ragent",
-}, {
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Transpiles SDK to be compatible with IE11 (increases bundle size)
-  transpileClientSDK: true,
-
-  // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-  tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-});
+export default nextConfig;
