2b5f6a401d1f6f34fbdd228c3af82c813fb2f85d
Satyam Tiwary <satyam@technoculture.io>
2023-06-12 00:07:28 +0530

    build issue

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,9 +5,5 @@ export default async function Page() {
   const user = await currentUser();
   const userId = user?.id;
 
-  if (!userId) {
-    redirect("/sign-in");
-  }
-
   redirect(`/${userId}`);
 }
