ffdafa242cda25f5ffcf90f4ebc6580f60308ce5
PrinceBaghel258025 <pkbghl90@gmail.com>
2023-07-11 12:34:42 +0530

    fix: making src/app/[uid]/error.tsx client again

--- src/app/[uid]/error.tsx
+++ src/app/[uid]/error.tsx
@@ -1,4 +1,6 @@
-export default async function Err() {
+"use client";
+
+export default function Err() {
   return (
     <div className="grid grid-cols-1 gap-2">
       <div className="grid md:grid-cols-4 gap-2">Error: User not found</div>
