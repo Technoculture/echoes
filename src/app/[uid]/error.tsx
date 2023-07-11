00fdcddfc856a98fd693179d553e940c506ca3d9
PrinceBaghel258025 <pkbghl90@gmail.com>
2023-07-11 12:17:47 +0530

    fix: tried to make src/app/[uid] a server component

--- src/app/[uid]/error.tsx
+++ src/app/[uid]/error.tsx
@@ -1,4 +1,4 @@
-export default function Err() {
+export default async function Err() {
   return (
     <div className="grid grid-cols-1 gap-2">
       <div className="grid md:grid-cols-4 gap-2">Error: User not found</div>
