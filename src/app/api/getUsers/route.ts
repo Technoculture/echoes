60e6746b6000af979353e8a31e06f9549a454b97
PrinceBaghel258025 <pkbghl90@gmail.com>
2023-07-15 17:45:57 +0530

    update: initialization for user avatars

---
+++ src/app/api/getUsers/route.ts
@@ -0,0 +1,15 @@
+import { clerkClient } from "@clerk/nextjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const ids = body?.ids;
+
+  const users = await clerkClient.users.getUserList({ userId: ids });
+  const requiredData = users.map((user) => ({
+    id: user.id,
+    img: user.imageUrl,
+  }));
+
+  return new NextResponse(JSON.stringify({ users: requiredData }));
+}
--- src/components/chatcard.tsx
+++ src/components/chatcard.tsx
@@ -2,7 +2,7 @@
 import { Chat } from "@/lib/db/schema";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { buttonVariants } from "./button";
 import {
   Card,
@@ -13,6 +13,7 @@ import {
   CardFooter,
 } from "./card";
 import { ChatLog } from "@/lib/types";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/avatar";
 
 type Props = {
   chat: Chat;
@@ -27,6 +28,30 @@ const Chatcard = ({ chat, uid, org_id }: Props) => {
     });
   };
 
+  // generate Avatar's of all the users
+  // filter all the distinct users from the chat.messages.log => will get all the user's id
+  // have to get the user's list and then again filter the users of particular Chat
+  // then from the imgUrl on the user object generate the image
+
+  const [users, setUsers] = useState([] as { id: string; img: string }[]);
+
+  useEffect(() => {
+    const getUsers = async (ids: Array<string>) => {
+      const users = await fetch("/api/getUsers", {
+        method: "POST",
+        body: JSON.stringify({ ids: ids }),
+      });
+      const data = await users.json();
+      console.log("data", data);
+      setUsers(data.users);
+    };
+
+    const ids = getUserIdList(chat.messages as string);
+    if (ids.length) {
+      getUsers(ids);
+    }
+  }, []);
+
   return (
     <Card>
       <CardHeader>
@@ -36,20 +61,32 @@ const Chatcard = ({ chat, uid, org_id }: Props) => {
           <b>{getChatCreator(chat.messages as string)}</b>
         </CardDescription>
       </CardHeader>
-      <CardContent>
-        {chat.title === "" ? (
-          <p>
-            No title{" "}
-            <button
-              className={buttonVariants({ variant: "outline" })}
-              onClick={generateTitle}
-            >
-              Generate
-            </button>{" "}
-          </p>
-        ) : (
-          chat.title
-        )}
+      <CardContent className="">
+        <p className="h-24 overflow-y-hidden">
+          {chat.title === "" ? (
+            <p>
+              No title{" "}
+              <button
+                className={buttonVariants({ variant: "outline" })}
+                onClick={generateTitle}
+              >
+                Generate
+              </button>{" "}
+            </p>
+          ) : (
+            chat.title
+          )}
+        </p>
+        <div className="flex ">
+          {users.length > 0
+            ? users.map((user) => (
+                <Avatar className="mr-2 w-9 h-9" key={user.id}>
+                  <AvatarImage src={user?.img} alt="@shadcn" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              ))
+            : "No Data Saved"}
+        </div>
       </CardContent>
       <CardFooter className="w-full">
         <Link
@@ -81,4 +118,18 @@ const getChatCreator = (chatMessages: string): string => {
     ? firstEntry.name.split(",")[0]
     : "Unknown Creator";
   return creator;
+};
+
+const getUserIdList = (chatMessages: string): Array<string> => {
+  const chatArray = (JSON.parse(chatMessages as string) as ChatLog)?.log;
+  const ArrayContainingUsers = chatArray.filter((chat) =>
+    chat.name !== "" ? chat.name : null,
+  );
+  const ids = ArrayContainingUsers.map((usr) => {
+    const split = usr.name.split(",");
+    return split.length > 0 ? split[1] : null;
+  });
+  const filteredIds = ids.filter((id) => id !== undefined);
+  console.log("ArrayContainingUsers", filteredIds);
+  return filteredIds as Array<string>;
 };
