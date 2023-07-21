39dba6bac8dec8eeba75e3fe196333d2e923dc84
Prince86 <pkbghl90@gmail.com>
2023-07-21 14:04:27 +0530

    removed export from getConversations

--- src/app/[uid]/page.tsx
+++ src/app/[uid]/page.tsx
@@ -65,7 +65,7 @@ export default async function Page({ params }: { params: { uid: string } }) {
   );
 }
 
-export const getConversations = async ({
+const getConversations = async ({
   orgId,
   offset = 0,
 }: {
