2b729621a35b0b8d5767c214c66a0de64dd47ea0
Prince86 <pkbghl90@gmail.com>
2023-08-03 14:53:09 +0530

    update: sentry initialization

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 import "./src/app/env.mjs";
-
+import { withSentryConfig } from "@sentry/nextjs"
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -17,4 +17,32 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withSentryConfig(withSentryConfig(nextConfig), {
+// For all available options, see:
+// https://github.com/getsentry/sentry-webpack-plugin#options
+
+// Suppresses source map uploading logs during build
+silent: true,
+authToken: process.env.SENTRY_AUTH_TOKEN,
+org: "prince-baghel",
+project: "echoes",
+hideSourceMaps: true,
+}, {
+// For all available options, see:
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+// Upload a larger set of source maps for prettier stack traces (increases build time)
+widenClientFileUpload: true,
+
+// Transpiles SDK to be compatible with IE11 (increases bundle size)
+transpileClientSDK: true,
+
+// Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+tunnelRoute: "/monitoring",
+
+// Hides source maps from generated client bundles
+hideSourceMaps: true,
+
+// Automatically tree-shake Sentry logger statements to reduce bundle size
+disableLogger: true,
+});
