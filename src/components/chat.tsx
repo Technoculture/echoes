ace04919ef59de991edca3f3c24c768103a0e2ae
PrinceBaghel258025 <pkbghl90@gmail.com>
2023-08-12 16:04:23 +0530

    improved scroll helpers and rebased to dev

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -5,6 +5,8 @@ import { ChatEntry, ChatLog } from "@/lib/types";
 import InputBar from "@/components/inputBar";
 import { Message, useChat } from "ai/react";
 import { useMutation } from "../../liveblocks.config";
+import { Button } from "@/components/button";
+import { ArrowCircleDown, ArrowCircleUp } from "@phosphor-icons/react";
 
 interface ChatProps {
   orgId: string;
@@ -20,6 +22,8 @@ export default function Chat(props: ChatProps) {
     storage.set("chat", data);
   }, []);
 
+  const [mouseDownTime, setMouseDownTime] = useState<number | null>(null);
+  const [showScrollButton, setShowScrollButton] = useState<boolean>(false);
   const [isFast, setIsFast] = useState<boolean>(true);
   const {
     messages,
@@ -48,6 +52,43 @@ export default function Chat(props: ChatProps) {
       updateRoomData(messages);
     }
   }, [messages]);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+  const scrollToBottom = () => {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100 && !showScrollButton) {
+        setShowScrollButton(() => true);
+      } else {
+        setShowScrollButton(() => false);
+      }
+    };
+    window?.addEventListener("scroll", handleScroll);
+    return () => {
+      window?.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const onmousedown = () => {
+    setMouseDownTime(Date.now());
+  };
+
+  const onmouseup = (goto: string) => {
+    const mouseUpTime = Date.now();
+    if (mouseDownTime) {
+      const timeHeld = mouseUpTime - mouseDownTime;
+      setMouseDownTime(null); // Reset the mouseDownTime
+      if (timeHeld > 1000) {
+        goto === "top" ? scrollToTop() : scrollToBottom();
+      }
+      console.log(`The button was held for ${timeHeld} milliseconds.`);
+    }
+  };
 
   return (
     <div className="grid grig-cols-1 gap-1">
@@ -86,6 +127,26 @@ export default function Chat(props: ChatProps) {
         setInput={setInput}
         append={append}
       />
+      <Button
+        onMouseDown={() => onmousedown()}
+        onMouseUp={() => onmouseup("top")}
+        className={`${
+          showScrollButton ? "fixed" : "hidden"
+        } w-12 p-0  rounded-full right-4 bottom-8  `}
+        variant={"ghost"}
+      >
+        <ArrowCircleUp size={32} />
+      </Button>
+      <Button
+        onMouseDown={() => onmousedown()}
+        onMouseUp={() => onmouseup("bottom")}
+        className={`${
+          !showScrollButton ? "fixed" : "hidden"
+        } w-12 p-0  rounded-full right-4 bottom-8  `}
+        variant={"ghost"}
+      >
+        <ArrowCircleDown size={32} />
+      </Button>
     </div>
   );
 }
