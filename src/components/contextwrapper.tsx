bd0d9694bee5d8a434e62e4f2debd17d30330467
Prince86 <pkbghl90@gmail.com>
2023-08-23 12:44:15 +0530

    triggring contextmenu on selection completion

--- src/components/contextwrapper.tsx
+++ src/components/contextwrapper.tsx
@@ -7,7 +7,7 @@ import {
 } from "@/components/contextmenu";
 import { useTextSelection } from "@mantine/hooks";
 import { ChatRequestOptions, CreateMessage, Message } from "ai";
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 
 import { PromptTemplate } from "langchain/prompts";
 import { predefinedPrompts } from "@/utils/constants";
@@ -55,40 +55,80 @@ export function ContextWrapper(props: Props) {
       props.append(message as Message);
     }
   };
+  const divRef = useRef<HTMLDivElement>(null);
+  const triggerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    let timeout: any;
+    const handleMouseUp = (e: any) => {
+      console.log("running handle mouse up", e);
+      timeout = setTimeout(() => {
+        console.log("log from timeout", selection?.toString());
+        // if(selection && selection?.toString().length > 0){
+        const event = new Event("contextmenu", {
+          bubbles: true,
+          composed: false,
+        });
+        // event.preventDefault()
+        // var ev = document.createEvent('HTMLEvents');
+        // ev.initEvent('contextmenu', true, false);
+        // element.dispatchEvent(ev);
+        triggerRef?.current?.dispatchEvent(event);
+        // const dblclickevent = new Event("dblclick")
+        // dblclickevent.preventDefault()
+        // dblclickevent.stopPropagation()
+        // triggerRef?.current?.ondblclick(dblclickevent)
+        // }
+      }, 200);
+      // return clearTimeout(timeout)
+    };
+
+    const el = divRef.current;
+    if (el) {
+      el.addEventListener("mouseup", handleMouseUp);
+    }
+    return () => {
+      clearTimeout(timeout);
+      el?.removeEventListener("mouse", handleMouseUp);
+    };
+  }, []);
 
   return (
-    <ContextMenu>
-      <ContextMenuTrigger
-        disabled={selection && selection.toString().length > 0 ? false : true}
-      >
-        {props.children}
-      </ContextMenuTrigger>
-      <ContextMenuContent className={`w-64`}>
-        <ContextMenuItem onClick={(e) => handleSubmit(e, "factCheck")} inset>
-          Is this true?
-          <ContextMenuShortcut>⇧⌘s</ContextMenuShortcut>
-        </ContextMenuItem>
-        <ContextMenuItem onClick={(e) => handleSubmit(e, "explain")} inset>
-          Explain This
-          <ContextMenuShortcut>⇧⌘S</ContextMenuShortcut>
-        </ContextMenuItem>
-        <ContextMenuItem onClick={(e) => handleSubmit(e, "elaborate")} inset>
-          Elaborate
-          <ContextMenuShortcut>⇧⌘E</ContextMenuShortcut>
-        </ContextMenuItem>
-        <ContextMenuItem onClick={(e) => handleSubmit(e, "criticise")} inset>
-          Analyse this Critically
-          <ContextMenuShortcut>⇧⌘C</ContextMenuShortcut>
-        </ContextMenuItem>
-        <ContextMenuItem onClick={(e) => handleSubmit(e, "examples")} inset>
-          I need Examples
-          <ContextMenuShortcut>⇧⌘E</ContextMenuShortcut>
-        </ContextMenuItem>
-        <ContextMenuItem onClick={(e) => handleSubmit(e, "references")} inset>
-          I need References
-          <ContextMenuShortcut>⇧⌘R</ContextMenuShortcut>
-        </ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+    <div ref={divRef}>
+      <ContextMenu>
+        <ContextMenuTrigger
+          ref={triggerRef}
+          disabled={selection && selection.toString().length > 0 ? false : true}
+        >
+          {props.children}
+        </ContextMenuTrigger>
+        <ContextMenuContent className={`w-64`}>
+          <ContextMenuItem onClick={(e) => handleSubmit(e, "factCheck")} inset>
+            Is this true?
+            <ContextMenuShortcut>⇧⌘s</ContextMenuShortcut>
+          </ContextMenuItem>
+          <ContextMenuItem onClick={(e) => handleSubmit(e, "explain")} inset>
+            Explain This
+            <ContextMenuShortcut>⇧⌘S</ContextMenuShortcut>
+          </ContextMenuItem>
+          <ContextMenuItem onClick={(e) => handleSubmit(e, "elaborate")} inset>
+            Elaborate
+            <ContextMenuShortcut>⇧⌘E</ContextMenuShortcut>
+          </ContextMenuItem>
+          <ContextMenuItem onClick={(e) => handleSubmit(e, "criticise")} inset>
+            Analyse this Critically
+            <ContextMenuShortcut>⇧⌘C</ContextMenuShortcut>
+          </ContextMenuItem>
+          <ContextMenuItem onClick={(e) => handleSubmit(e, "examples")} inset>
+            I need Examples
+            <ContextMenuShortcut>⇧⌘E</ContextMenuShortcut>
+          </ContextMenuItem>
+          <ContextMenuItem onClick={(e) => handleSubmit(e, "references")} inset>
+            I need References
+            <ContextMenuShortcut>⇧⌘R</ContextMenuShortcut>
+          </ContextMenuItem>
+        </ContextMenuContent>
+      </ContextMenu>
+    </div>
   );
 }
