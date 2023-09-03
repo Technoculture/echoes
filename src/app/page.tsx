9b78cda8da24d19dc4e91d74955cb3a36f1ef102
Satyam Tiwary <satyam@technoculture.io>
2023-09-03 15:58:36 +0530

    ui: fix footer responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,39 @@ import { useEffect } from "react";
 import { Header } from "@/components/header";
 import { Button } from "@/components/button";
 import Link from "next/link";
-import Image from "next/image";
+import { Widget } from "@typeform/embed-react";
+//import Image from "next/image";
 import {
   motion,
   AnimatePresence,
-  MotionConfig,
+  //MotionConfig,
   useAnimation,
 } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { Key, LayoutDashboard } from "lucide-react";
 
-const squareVariants = {
+const handleSmoothScroll = (): void => {
+  if (typeof window !== "undefined") {
+    const hashId = window.location.hash;
+
+    console.log({ location: window.location, hashId });
+
+    if (hashId) {
+      const element = document.querySelector(hashId);
+      console.log({ element });
+
+      if (element) {
+        element.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+          inline: "nearest",
+        });
+      }
+    }
+  }
+};
+
+const footerAnimationStates = {
   visible: { opacity: 1, y: 0, transition: { duration: 1 } },
   hidden: { opacity: 0, y: 100 },
 };
@@ -32,95 +54,111 @@ export default function Home() {
 
   return (
     <div>
-      <div className="h-screen relative">
-        <Header variant="sticky">
-          <Button
-            className="mr-4 h-[32px]"
-            variant="secondary"
-            asChild
-          ></Button>
-        </Header>
-        <div className="flex flex-col flex-grow p-20 h-screen justify-center items-center gap-2 text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-foreground">
-            Hello Innovator,
-          </h1>
-          <h1 className="text-2xl text-popover-foreground flex-wrap w-72 text-center">
-            Hyper-accelerate your research.
-          </h1>
-          <div className="grid md:grid-col-2 gap-4 sm:grid-col-1 p-4">
-            <Button>
-              <LayoutDashboard className="mr-2 w-4 h-4" />
-              <Link href="/dashboard">Dashboard</Link>
-            </Button>
-            <Button variant="secondary">
-              <Key className="mr-2 w-4 h-4" />
-              Request Access
-            </Button>
-          </div>
-        </div>
-      </div>
-      <motion.div
-        ref={ref}
-        animate={controls}
-        initial="hidden"
-        variants={squareVariants}
-        className="p-20 grid md:grid-cols-3 sm:grid-cols-1 gap-4 bg-slate-950 border border-t-1 border-slate-900"
-      >
-        <div className="p-4 flex flex-col space-y-4">
-          <h1 className="font-bold mb-4">Trusted By Innovators</h1>
-          <Link
-            href="https://technoculture.io"
-            className="text-sm text-popover-foreground"
-          >
-            Technoculture, Inc.
-          </Link>
-          <Link
-            href="https://exrna.com"
-            className="text-sm text-popover-foreground"
-          >
-            ExRNA Therapeutics
-          </Link>
-          <Link
-            href="https://vvbiotech.com"
-            className="text-sm text-popover-foreground"
-          >
-            VV Biotech
-          </Link>
-        </div>
-        <div className="p-4 flex flex-col space-y-4">
-          <h1 className="font-bold mb-4">Built With</h1>
-          <Link
-            href="https://openai.com/"
-            className="text-sm text-popover-foreground"
-          >
-            OpenAI
-          </Link>
-          <Link
-            href="https://modal.com/"
-            className="text-sm text-popover-foreground"
-          >
-            Modal Labs
-          </Link>
-        </div>
-        <div className="p-4 flex flex-col space-y-2">
-          <h1 className="font-bold mb-4">Integrations</h1>
-          <Link
-            href="https://openoligo.com/"
-            className="text-sm text-popover-foreground"
-          >
-            OpenOligo
-          </Link>
-        </div>
-        <div className="p-4 col-span-3 justify-end">
-          <hr className="mb-8 border-slate-900" />
-          <div className="flex justify-between">
-            <h1 className="text-sm text-popover-foreground">
-              © 2023 Technoculture Research
+      <AnimatePresence onExitComplete={handleSmoothScroll}>
+        <div className="h-screen relative">
+          <Header variant="sticky">
+            <Button
+              className="mr-4 h-[32px]"
+              variant="secondary"
+              asChild
+            ></Button>
+          </Header>
+          <div className="flex flex-col flex-grow p-20 h-screen justify-center items-center gap-2 text-center">
+            <h1 className="text-4xl font-bold tracking-tight text-foreground">
+              Hello Innovator,
             </h1>
-            <h1 className="text-xl text-popover-foreground">🇮🇳</h1>
+            <h1 className="text-2xl text-popover-foreground flex-wrap w-72 text-center">
+              Let's hyper-accelerate your research.
+            </h1>
+            <div className="grid md:grid-col-2 gap-4 sm:grid-col-1 p-4">
+              <Button>
+                <LayoutDashboard className="mr-2 w-4 h-4" />
+                <Link href="/dashboard">Dashboard</Link>
+              </Button>
+              <Button variant="secondary">
+                <Key className="mr-2 w-4 h-4" />
+                <Link href="/#requestaccess">Request Access</Link>
+              </Button>
+            </div>
           </div>
         </div>
-      </motion.div>
+      </AnimatePresence>
+
+      <AnimatePresence onExitComplete={handleSmoothScroll}>
+        <div
+          className="h-screen flex flex-col justify-center items-center bg-gradient-to-b from-slate-950 via-black to-black -z-20"
+          id="requestaccess"
+        >
+          <Widget id="H4H0D2hi" className="w-min-72 w-3/4 h-3/4 -z-2" />
+        </div>
+      </AnimatePresence>
+
+      <div className="md:p-20 p-12 bg-slate-950 border border-t-1 border-slate-900">
+        <motion.div
+          ref={ref}
+          animate={controls}
+          initial="hidden"
+          variants={footerAnimationStates}
+          className="grid md:grid-cols-3 sm:grid-cols-1 gap-4 sm:gap-8"
+        >
+          <div className="p-4 flex flex-col space-y-4">
+            <h1 className="font-bold mb-4">Trusted By Innovators</h1>
+            <Link
+              href="https://technoculture.io"
+              className="text-sm text-popover-foreground"
+            >
+              Technoculture, Inc.
+            </Link>
+            <Link
+              href="https://exrna.com"
+              className="text-sm text-popover-foreground"
+            >
+              ExRNA Therapeutics
+            </Link>
+            <Link
+              href="https://vvbiotech.com"
+              className="text-sm text-popover-foreground"
+            >
+              VV Biotech
+            </Link>
+          </div>
+
+          <div className="p-4 flex flex-col space-y-4">
+            <h1 className="font-bold mb-4">Built With</h1>
+            <Link
+              href="https://openai.com/"
+              className="text-sm text-popover-foreground"
+            >
+              OpenAI
+            </Link>
+            <Link
+              href="https://modal.com/"
+              className="text-sm text-popover-foreground"
+            >
+              Modal Labs
+            </Link>
+          </div>
+
+          <div className="p-4 flex flex-col space-y-2">
+            <h1 className="font-bold mb-4">Integrations</h1>
+            <Link
+              href="https://openoligo.com/"
+              className="text-sm text-popover-foreground"
+            >
+              OpenOligo
+            </Link>
+          </div>
+          <div className="p-4 col-span-1 md:col-span-3 justify-end">
+            <hr className="mb-8 border-slate-900" />
+            <div className="flex justify-between">
+              <h1 className="text-sm text-popover-foreground">
+                © 2023 Technoculture Research
+              </h1>
+              <h1 className="text-xl text-popover-foreground">🇮🇳</h1>
+            </div>
+          </div>
+        </motion.div>
+      </div>
     </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -25,41 +25,39 @@ export interface HeaderProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof headerVariants> {}
 
-const Header = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, variant, ...props }: HeaderProps, ref) => {
-  return (
-  <header
-    className={cn(
-      headerVariants({ variant, className }),
-      "",
-    )}
-    ref={ref}
-    {...props}
-  >
-    <div className="flex justify-between p-5">
-      <Link
-        href="/"
-        className="gap-2 flex items-center cursor-pointer h-[32px]"
+const Header = React.forwardRef<HTMLDivElement, HeaderProps>(
+  ({ className, variant, ...props }: HeaderProps = {}, ref) => {
+    return (
+      <header
+        className={cn(headerVariants({ variant, className }), "")}
+        ref={ref}
+        {...props}
       >
-        <Image
-          src={logo}
-          alt="logo"
-          className="w-6 h-6 border-gray-700 border-2"
-        />
-        <h1 className="text-gray-200 align-middle">Echoes</h1>
-      </Link>
-      <SignedIn>
-        <div className="flex">
-          {props.children}
-          <UserButton />
+        <div className="flex justify-between p-5">
+          <Link
+            href="/"
+            className="gap-2 flex items-center cursor-pointer h-[32px]"
+          >
+            <Image
+              src={logo}
+              alt="logo"
+              className="w-6 h-6 border-gray-700 border-2"
+            />
+            <h1 className="text-gray-200 align-middle">Echoes</h1>
+          </Link>
+          <SignedIn>
+            <div className="flex">
+              {props.children}
+              <UserButton />
+            </div>
+          </SignedIn>
+          <SignedOut></SignedOut>
         </div>
-      </SignedIn>
-      <SignedOut></SignedOut>
-    </div>
-  </header>
+      </header>
+    );
+  },
 );
-});
+
+Header.displayName = "Header";
 
 export { Header };
