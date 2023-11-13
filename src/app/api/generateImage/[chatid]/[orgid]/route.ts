018ad515821bbea3ab2078503b9c03b5f8deea19
Prince86 <pkbghl90@gmail.com>
2023-11-13 17:28:39 +0530

    fix: timed out

--- src/app/api/generateImage/[chatid]/[orgid]/route.ts
+++ src/app/api/generateImage/[chatid]/[orgid]/route.ts
@@ -4,6 +4,7 @@ import { chats } from "@/lib/db/schema";
 import { NextResponse } from "next/server";
 import { generateChatImage } from "@/utils/apiHelper";
 export const revalidate = 0; // disable cache
+export const maxDuration = 60; 
 
 export async function POST(
   request: Request,
@@ -14,7 +15,7 @@ export async function POST(
   const body = await request.json();
   console.log("prompt message", body.chatTitle);
   const image_url = await generateChatImage(body.chatTitle as string, chatId);
-  
+
   await db
     .update(chats)
     .set({ image_url: image_url })
