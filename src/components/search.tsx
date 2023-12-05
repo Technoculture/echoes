79f4dde15b4553392ece470ee73650762565954d
Prince Baghel <pkbghl90@gmail.com>
2023-12-05 06:08:59 +0530

    fix: remove 0

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -84,40 +84,47 @@ const Search = (props: Props) => {
           />
           <CommandList>
             <CommandEmpty>No results found.</CommandEmpty>
-            {results.length &&
-              results.map((result: any) => (
-                <Link
-                  href={
-                    result.id
-                      ? `/usr/${props.orgSlug}/chat/${result.chatId}/#${result.id}`
-                      : `/usr/${props.orgSlug}/chat/${result.chatId}`
-                  } // needs to be updated
-                  onClick={() => setOpen(false)}
-                  key={result.objectID}
-                >
-                  <CommandItem className="pointer-events-auto w-full">
-                    <div className="select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none text-accent-foreground transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground flex flex-row justify-between items-center w-full">
-                      <div className="flex flex-col gap-2">
-                        <div className="">
-                          {result.chatTitle
-                            .replace('"', "")
-                            .replace('"', "")
-                            .split(" ")
-                            .slice(0, 5)
-                            .join(" ")}
-                          <p className=" ml-1 line-clamp-2 text-sm leading-snug text-muted-foreground">
-                            {result.content.split(" ").slice(0, 5).join(" ")}
+            {results.length
+              ? results.map((result: any) => (
+                  <Link
+                    href={
+                      result.id
+                        ? `/usr/${props.orgSlug}/chat/${result.chatId}/#${result.id}`
+                        : `/usr/${props.orgSlug}/chat/${result.chatId}`
+                    } // needs to be updated
+                    onClick={() => setOpen(false)}
+                    key={result.objectID}
+                  >
+                    <CommandItem className="pointer-events-auto w-full">
+                      <div className="select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none text-accent-foreground transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground flex flex-row justify-between items-center w-full">
+                        <div className="flex flex-col gap-2">
+                          <div className="">
+                            {result.chatTitle
+                              .replace('"', "")
+                              .replace('"', "")
+                              .split(" ")
+                              .slice(0, 5)
+                              .join(" ")}
+                            <p className=" ml-1 line-clamp-2 text-sm leading-snug text-muted-foreground">
+                              {result.content
+                                ? result.content
+                                    .split(" ")
+                                    .slice(0, 5)
+                                    .join(" ")
+                                : null}
+                            </p>
+                          </div>
+                          <p className="line-clamp-2 text-xs leading-snug text-muted-foreground">
+                            Last Updated At :{" "}
+                            {timestampToDate(result.updatedAt)}
                           </p>
                         </div>
-                        <p className="line-clamp-2 text-xs leading-snug text-muted-foreground">
-                          Last Updated At : {timestampToDate(result.updatedAt)}
-                        </p>
+                        <UserAvatar role={result.role} userData={result.name} />
                       </div>
-                      <UserAvatar role={result.role} userData={result.name} />
-                    </div>
-                  </CommandItem>
-                </Link>
-              ))}
+                    </CommandItem>
+                  </Link>
+                ))
+              : null}
           </CommandList>
         </Command>
       </DialogContent>
